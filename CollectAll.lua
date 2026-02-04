b2bcf56a259ee1fbee1abab4bd4f0daf8d64ef27
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-02-04 12:55:18 +0000

    Update CollectAll.lua

--- CollectAll.lua
+++ CollectAll.lua
@@ -1,1 +1,25 @@
+local bases = workspace:WaitForChild("Bases")
 
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local head = char:WaitForChild("Head")
+
+for _, base in ipairs(bases:GetChildren()) do
+	task.spawn(function()
+		local config = base:FindFirstChild("Configuration")
+		if not config then return end
+
+		local basePlayer = config.Player.Value
+		if basePlayer ~= plr then return end
+
+		for _, slot in ipairs(base.Slots:GetChildren()) do
+			local collect = slot:FindFirstChild("Collect")
+			if not collect then continue end
+
+			local touch = collect:FindFirstChildOfClass("TouchTransmitter")
+			if touch then
+				print("hi")
+			end
+		end
+	end)
+end
