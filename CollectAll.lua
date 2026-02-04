0dcd80d07bcca5f4420f1904273a4947b211ea3e
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-02-04 13:01:35 +0000

    Update CollectAll.lua

--- CollectAll.lua
+++ CollectAll.lua
@@ -2,7 +2,7 @@ local bases = workspace:WaitForChild("Bases")
 
 local plr = game.Players.LocalPlayer
 local char = plr.Character or plr.CharacterAdded:Wait()
-local head = char:WaitForChild("Head")
+local head = char:WaitForChild("HumanoidRootPart")
 
 for _, base in ipairs(bases:GetChildren()) do
 	task.spawn(function()
@@ -15,10 +15,10 @@ for _, base in ipairs(bases:GetChildren()) do
 		for _, slot in ipairs(base.Slots:GetChildren()) do
 			local collect = slot:FindFirstChild("Collect")
 			if not collect then continue end
-
 			local touch = collect:FindFirstChildOfClass("TouchTransmitter")
 			if touch then
-				print("hi")
+				firetouchinterest(collect, head, 0)
+				firetouchinterest(collect, head, 1)
 			end
 		end
 	end)
